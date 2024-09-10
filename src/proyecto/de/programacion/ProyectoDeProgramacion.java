2e242e706fdd41dcfb16a822b74fb2874f351290
Ponche14 <jdp142006@unitec.edu>
2024-09-09 22:12:26 -0600

    Commit

---
+++ src/proyecto/de/programacion/ProyectoDeProgramacion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.de.programacion;
+
+/**
+ *
+ * @author jdp14
+ */
+public class ProyectoDeProgramacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
